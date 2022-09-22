8183c6a03f81c6f0970dcf454cec10cafab81d28
Emmattzon <emmattzontech@gmail.com>
2022-09-22 19:47:15 +0100

    initial

---
+++ base/static/base/js/dropdown.js
@@ -0,0 +1,16 @@
+function myToggle(){
+    document.getElementById("dropdown").classList.toggle("dropdown--show");
+
+}
+
+window.onclick = function(event){
+    if (!event.target.matches('.toggle')){
+        var dropdowns = document.getElementsByClassName("dropdown");
+        for(var i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('dropdown--show')){
+                openDropdown.classList.remove('dropdown--show');
+            }
+        }
+    }
+};
